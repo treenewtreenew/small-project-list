3ed2bce2b28224b1f4e93343748eb69a85c5669d
treenewtreenew <treenewtreenew@gmail.com>
2023-02-19 22:58:48 +0900

    springboot react

---
+++ java/crud-practice/src/main/java/com/spring3react/crudpractice/CrudPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring3react.crudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPracticeApplication.class, args);
+	}
+
+}
---
+++ java/crud-practice/src/test/java/com/spring3react/crudpractice/CrudPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring3react.crudpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
