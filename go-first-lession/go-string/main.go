89a92073bbe92c48374f7d399af2d9ee53a3b4cb
treenewtreenew <treenewtreenew@gmail.com>
2024-04-10 23:10:43 +0900

    string

---
+++ go-first-lession/go-string/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	//string_cannot_be_changed()
+	//string_content()
+	//rune_in_go()
+	//string_literal()
+	//encodeRune()
+	decodeRune()
+}
+
+// concurrent safety
+// remove \0
+// get length of string is quick, Constant time complexity
+func string_cannot_be_changed() {
+	var s string = "hello"
+	// s[0] = 'k' // string in go can not be changed
+	s = "gopher"
+
+	s = `,_---~~~~~----._
+	_,,_,*^____ _____*g*\"*,--,
+	/ __/ /' ^. / \ ^@q f
+	[ @f | @)) | | @)) l 0 _/
+	\/ \~____ / __ \_____/ \
+	| _l__l_ I
+	} [______] I
+	] | | | |
+	] ~ ~ |
+	| |
+	| |`
+	fmt.Println(s)
+}
+
+func string_content() {
+	var s string = "中国人"
+	fmt.Printf("the length of s = %d\n", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("0x%x", s[i])
+	}
+	fmt.Printf("\n")
+
+	fmt.Println("the character count in s is ", utf8.RuneCountInString(s))
+	for _, c := range s {
+		fmt.Printf("0x%x", c)
+	}
+	fmt.Printf("\n")
+}
+
+func rune_in_go() {
+	var a rune = 'a'
+	var b rune = '中'
+	var c rune = '\n'
+	var d rune = '\''
+	fmt.Println(string(a), string(b), string(c), string(d))
+
+	a1 := '\u4e2d'
+	b1 := '\U00004e2d'
+	c1 := '\u0027'
+	fmt.Println(string(a1), string(b1), string(c1))
+}
+
+func string_literal() {
+	a := "abc\n"
+	b := "中国人"
+	c := "\u4e2d\u56fd\u4eba"
+	d := "\U00004e2d\U000056fd\U00004eba"
+	e := "中\u56fd\u4eba"
+	f := "\xe4\xb8\xad\xe5\x9b\xbd\xe4\xba\xba"
+	fmt.Println(a, b, c, d, e, f)
+}
+
+// rune -> []byte
+func encodeRune() {
+	var r rune = 0x4E2D
+	fmt.Printf("the unicode character is %c\n", r)
+	buf := make([]byte, 3)
+	_ = utf8.EncodeRune(buf, r)
+	fmt.Printf("utf8 representation is 0x%X\n", buf)
+}
+
+// []byte -> rune
+func decodeRune() {
+	var buf = []byte{0xE4, 0xB8, 0xAD}
+	r, _ := utf8.DecodeRune(buf)
+	fmt.Printf("the unicode character after decoding [0xE4, 0xB8, 0xAD] is %s\n", string(r))
+}
