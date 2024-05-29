e56051a2661251be0d2d567d23f6d7d17e2831e8
treenewtreenew <treenewtreenew@gmail.com>
2024-05-29 22:09:52 +0900

    go 21

--- go-first-lession/go-21/main.go
+++ go-first-lession/go-21/main.go
@@ -5,6 +5,18 @@ var foo = func() {
 	println("this is a func definition")
 }
 
+// variable parameters declaration
+func vFunc(i int, s string, a ...interface{}) (n int, err error) {
+	println(i)
+	println(s)
+	for i, v := range a {
+		println(i)
+		println(v)
+	}
+	return 1, nil
+}
+
 func main() {
 	foo()
+	vFunc(1, "1", "a", "b", 2)
 }
