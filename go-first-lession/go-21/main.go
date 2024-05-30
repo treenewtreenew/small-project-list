8d40a5285a7c79884e10451dcc23b864fe2c43fb
treenewtreenew <treenewtreenew@gmail.com>
2024-05-30 22:04:27 +0900

    func

--- go-first-lession/go-21/main.go
+++ go-first-lession/go-21/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // function literal
 var foo = func() {
 	println("this is a func definition")
@@ -16,7 +21,32 @@ func vFunc(i int, s string, a ...interface{}) (n int, err error) {
 	return 1, nil
 }
 
+func greeting(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome, Gopher!\n")
+}
+
+func times(x int, y int) int {
+	return x * y
+}
+
+func partialTimes(x int) func(int) int {
+	return func(y int) int {
+		return times(x, y)
+	}
+}
+
 func main() {
 	foo()
 	vFunc(1, "1", "a", "b", 2)
+	// cannot use greeting (value of type func(w http.ResponseWriter, r *http.Request))
+	// as http.Handler value in argument to http.ListenAndServe:
+	// func(w http.ResponseWriter, r *http.Request) does not implement
+	// http.Handler (missing method ServeHTTP)
+	// http.ListenAndServe(":8080", greeting)
+	// below is correct
+	// http.ListenAndServe(":8080", http.HandlerFunc(greeting))
+	times2 := partialTimes(2)
+	println(times2(3))
+	time3 := partialTimes(3)
+	println(time3(3))
 }
