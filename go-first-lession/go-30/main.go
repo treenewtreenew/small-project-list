9c2ae4afc30a9ef573a5fd4fea1cbbe5eba2f630
treenewtreenew <treenewtreenew@gmail.com>
2024-08-02 23:50:49 +0900

    go 30

--- go-first-lession/go-30/main.go
+++ go-first-lession/go-30/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"os"
@@ -28,4 +29,31 @@ func main() {
 	if _, err := io.Copy(os.Stdout, lr); err != nil {
 		log.Fatal(err)
 	}
+
+	r1 := strings.NewReader("hello, gopher!\n")
+	r2 := CapReader(io.LimitReader(r1, 4))
+	if _, err := io.Copy(os.Stdout, r2); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CapReader(r io.Reader) io.Reader {
+	return &capitalizedReader{r: r}
+}
+
+type capitalizedReader struct {
+	r io.Reader
+}
+
+func (r *capitalizedReader) Read(p []byte) (int, error) {
+	n, err := r.r.Read(p)
+	if err != nil {
+		return 0, err
+	}
+
+	q := bytes.ToUpper(p)
+	for i, v := range q {
+		p[i] = v
+	}
+	return n, err
 }
